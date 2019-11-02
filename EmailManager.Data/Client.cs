adc4b9f13fa6ddffb24fce0cab9dfb65b8d7a458
Martu <martinpismymail@gmail.com>
2019-11-02 19:18:06 +0200

    Fixed version mismatch

--- EmailManager.Data/Client.cs
+++ EmailManager.Data/Client.cs
@@ -6,7 +6,7 @@ namespace EmailManager.Data
 {
     public class Client : User
     {
-        public override string PhoneNumber { get; set; }
+        public  override string PhoneNumber { get; set; }
 
         public string EGN { get; set; }
     }
