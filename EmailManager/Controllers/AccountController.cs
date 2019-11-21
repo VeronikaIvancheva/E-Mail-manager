759602d42247a08939cfdae04a0ab5ba1a097979
Martu <martinpismymail@gmail.com>
2019-11-21 13:57:31 +0200

    Seeded new Users

---
+++ EmailManager/Controllers/AccountController.cs
@@ -0,0 +1,33 @@
+﻿using EmailManager.Services.Contracts;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EmailManager.Controllers
+{
+    public class AccountController : Controller
+    {
+        private readonly IUserServices _user;
+
+        public AccountController(IUserServices user)
+        {
+            this._user = user;
+        }
+
+        [HttpGet]
+        public IActionResult Registration()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        [Authorize(Roles = "Manager")]
+        public async Task<IActionResult> Registration()
+
+
+    }
+}
